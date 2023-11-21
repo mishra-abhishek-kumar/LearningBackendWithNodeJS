a12b2303ec1dc3d4693c96d0bd2e3ad920ed7b0c
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-11-21 11:14:52 +0530

    exporting-importing modules-1

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 //Different ways to import in NodeJs
-const {multiply} = require('./utils');
+const {multiply, log} = require('./utils');
 const utils = require('./utils');
 
 console.log(multiply(2, 3));
 console.log(utils.multiply(3, 4));
+
+utils.log.error("this is just an info");
+log.warning("this is just an info");
--- utils.js
+++ utils.js
@@ -1,12 +1,21 @@
-
+//Exporting function
 const multiply = (a, b) => {
     return a*b;
 }
 
+//Exporting Object
+const log = {
+    warning: (info) => {
+        console.log('warning:', info);
+    },
+    error: (info) => {
+        console.log('error:', info);
+    }
+}
 
 
 //Different ways to export module
 
 //1st method
-module.exports = {multiply};
+module.exports = {multiply, log};
 
