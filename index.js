0f88e3366ec5f42072bd296d29da44555966ade2
mishra_abhishek_kumar <mishraabhishek0807@gmail.com>
2023-11-21 11:06:37 +0530

    Learning modules

---
+++ index.js
@@ -0,0 +1,6 @@
+//Different ways to import in NodeJs
+const {multiply} = require('./utils');
+const utils = require('./utils');
+
+console.log(multiply(2, 3));
+console.log(utils.multiply(3, 4));
---
+++ utils.js
@@ -0,0 +1,12 @@
+
+const multiply = (a, b) => {
+    return a*b;
+}
+
+
+
+//Different ways to export module
+
+//1st method
+module.exports = {multiply};
+
